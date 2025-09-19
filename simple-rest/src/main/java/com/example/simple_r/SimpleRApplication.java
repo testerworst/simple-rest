46e402915fb08b0d02d43ac255bd658a4a3a184c
subashpernandas <subashpstalin@gmail.com>
2025-09-19 19:25:37 +0530

    code pushed

---
+++ simple-rest/src/main/java/com/example/simple_r/SimpleRApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_r;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRApplication.class, args);
+	}
+
+}
---
+++ simple-rest/src/test/java/com/example/simple_r/SimpleRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_r;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
