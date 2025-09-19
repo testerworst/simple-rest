48540bc0f8ea3a03f166081a8468657fa7b9fef3
subashpernandas <subashpstalin@gmail.com>
2025-09-19 20:44:39 +0530

    log stmt add

--- src/main/java/com/example/simple_r/RestDataController.java
+++ src/main/java/com/example/simple_r/RestDataController.java
@@ -1,11 +1,14 @@
 package com.example.simple_r;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/map")
+@Slf4j
 public class RestDataController {
 
     @GetMapping("/get")
@@ -15,6 +18,8 @@ public class RestDataController {
 
     @GetMapping("/student")
     public StudentDTO studentDTO(){
+        log.info("hello");
         return new StudentDTO("subash.pernandas.savari", 32);
     }
+
 }
